766fa6f8039177369f4e94e9ea1ad175b2db4885
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-10 07:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if (string===lowercase) {
+    return "I can't hear you!"
+  } else if (string ===uppercase) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
